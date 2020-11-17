bedeeafc30e6d0d994359e5ed64a302431713c4e
Shykial <szymon.jeziorski@interia.pl>
2020-11-17 13:42:55 +0100

    re-adding incorrectly deleted timer decorator

--- decorators.py
+++ decorators.py
@@ -52,3 +52,13 @@ class Logger:
             return wrapper
 
         return inner
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        print(f'Function "{func.__name__}" took {time.time() - start_time} to execute')
+        return result
+
+    return wrapper
